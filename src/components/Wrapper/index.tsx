d7b14f0ca4de8d1a20ed546f975523b324558399
Zhdan <zhdanp95@mail.ru>
2024-11-19 23:24:39 +0300

    title, svg+-, div for logoblock

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 export const Background = styled.body`
+  position: fixed;
+  width: 100%;
+  height: 100%;
   background-image: url("/BG.png");
   display: flex;
-  justify-content: center;
-  align-items: center;
   margin: 0;
   flex-direction: column;
+  height: 100%;
+  background-size: contain;
+  background-position: 0 0;
+  background-repeat: no-repeat;
 `;
 type TWrapper = {
   children: React.ReactNode;
