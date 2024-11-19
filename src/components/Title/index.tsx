cffd286d641e692f7543d1eadd1741436b0e550e
Zhdan <zhdanp95@mail.ru>
2024-11-19 19:49:48 +0300

    make space, title and wrapper

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import React from "react";
+
+export const Title = styled.h1`
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 32px;
+  color: white;
+`;
